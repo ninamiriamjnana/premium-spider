f68200cd06b37fa28356b2ab33f68c374cc1fff2
ninamiriamjnana <nina.pohler@posteo.de>
2015-04-24 10:37:12 +0200

    Ich fange an, die Pipeline mit Peewee zu schreiben

--- pspider/pspider/pipelines.py
+++ pspider/pspider/pipelines.py
@@ -6,9 +6,29 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class PspiderPipeline(object):
-    def process_item(self, item, spider):
-        return item
 
 
-# example here http://stackoverflow.com/questions/10845839/writing-items-to-a-mysql-database-in-scrapy
+def process_item (self, item, spider):
+        
+        text=item['post']
+        timestamp=item['timestamp']
+        order=item['order']
+        user=item['user']
+        topic=item['topic']
+        try:
+            post=Post.get(Post.text==text, timestamp==timestamp)
+
+        except Post.DoesNotExist:
+            post=Post.create(text=text, timestamp=timestamp, order=order # ja und hier müsste ich aber halt die FKs reingeben!!
+
+        if item['user']:
+            # get AUTHOR
+				try:
+					user=User.get(User.name == item['user'])
+				
+				# no Author? create one!
+				except User.DoesNotExist:
+					user = User.create(name = item['user'])
+                
+        else:
+            raise DropItem("No user")
