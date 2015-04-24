6e0fe330f574addbcb77074071f33659b81dd7dd
ninamiriamjnana <nina.pohler@posteo.de>
2015-04-24 11:09:04 +0200

    added class PeeweePipeline rund um die process item funktion

--- pspider/pspider/pipelines.py
+++ pspider/pspider/pipelines.py
@@ -10,58 +10,60 @@ from scrapy.exceptions import DropItem
 from scrapy.http import Request
 
 
-def process_item (self, item, spider):
-        
-    text=item['post']
-    timestamp=item['timestamp']
-    order=item['order']
-    user=item['user']
-    topic=item['topic']
 
-   # als allererstes speicher ich die html seite
-  #get HTMLSite
-    try:
-        htmlsite=HTMLSite.get(HTMLSite.body == item['body'])
+class PeeweePipeline(object):
+    def process_item (self, item, spider):
+            
+        text=item['post']
+        timestamp=item['timestamp']
+        order=item['order']
+        user=item['user']
+        topic=item['topic']
+
+       # als allererstes speicher ich die html seite
+      #get HTMLSite
+        try:
+            htmlsite=HTMLSite.get(HTMLSite.body == item['body'])
 				
-	# no HTMLSite? create one!
-	except HTMLSite.DoesNotExist:
-	    htmlsite= HTMLSite.create(body = item['body'])  
+	    # no HTMLSite? create one!
+	    except HTMLSite.DoesNotExist:
+	        htmlsite= HTMLSite.create(body = item['body'])  
 
-   # ich fang mit denen an, von denen ich nachher die FKs brauche: topic und user  
-   # danach schreib ich alles andere rein
-        
-        
-        if item['user']:
-            # get USER
-				try:
-					user=User.get(User.name == item['user'])
+       # ich fang mit denen an, von denen ich nachher die FKs brauche: topic und user  
+       # danach schreib ich alles andere rein
+            
+            
+            if item['user']:
+                # get USER
+				    try:
+					    user=User.get(User.name == item['user'])
 				
-				# no USER? create one!
-				except User.DoesNotExist:
-					user = User.create(name = item['user'])
-            if item['topic']:
-                # get TOPIC
-				try:
-					topic=Topic.get(Topic.name == item['topic'])
+				    # no USER? create one!
+				    except User.DoesNotExist:
+					    user = User.create(name = item['user'])
+                if item['topic']:
+                    # get TOPIC
+				    try:
+					    topic=Topic.get(Topic.name == item['topic'])
 				
-				# no Topic? create one!
-				except Topic.DoesNotExist:
-					topic= Topic.create(name = itemitem['topic'])
-              
-                 try:
-                    post=Post.get(Post.text==text, timestamp==timestamp)
-
-                except Post.DoesNotExist:
-                    post=Post.create(text=text, timestamp=timestamp, order=order, topic=topic, user=user) #
-
+				    # no Topic? create one!
+				    except Topic.DoesNotExist:
+					    topic= Topic.create(name = itemitem['topic'])
                   
-            else:
-                raise DropItem("No topic")
-                
-        else:
-            raise DropItem("No user")
+                     try:
+                        post=Post.get(Post.text==text, timestamp==timestamp)
 
-     return item
+                    except Post.DoesNotExist:
+                        post=Post.create(text=text, timestamp=timestamp, order=order, topic=topic, user=user) #
+
+                      
+                else:
+                    raise DropItem("No topic")
+                    
+            else:
+                raise DropItem("No user")
+
+         return item
 
        
        
