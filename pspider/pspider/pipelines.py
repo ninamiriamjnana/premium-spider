0183a1e5f0806f964435664ac2ae3eda71295dc0
ninamiriamjnana <nina.pohler@posteo.de>
2015-04-24 12:21:50 +0200

    code stimmt jetzt aber es wird nichts in die datenbank geschrieben?

--- pspider/pspider/pipelines.py
+++ pspider/pspider/pipelines.py
@@ -24,50 +24,50 @@ class PeeweePipeline(object):
         body=item['body']
         url=item['url']
 
-       # als allererstes speicher ich die html seite
-      #get HTMLSite
+        # als allererstes speicher ich die html seite
+        #get HTMLSite
         try:
-            htmlpage=HTMLPage.get(HTMLPage.url== url)
+            htmlpage=HTMLPage.get(HTMLPage.url == url)
 				
 	    # no HTMLSite? create one!
-	    except HTMLPage.DoesNotExist:
-	        htmlpage= HTMLPage.create(body =body, url=url)  
+        except HTMLPage.DoesNotExist:
+	        htmlpage= HTMLPage.create(body=body, url=url)  
 
-       # ich fang mit denen an, von denen ich nachher die FKs brauche: topic und user  
-       # danach schreib ich alles andere rein
+        # ich fang mit denen an, von denen ich nachher die FKs brauche: topic und user  
+        # danach schreib ich alles andere rein**/"""
             
             
-            if item['user']:
-                # get USER
-				    try:
-					    user=User.get(User.name == item['user'])
-				
-				    # no USER? create one!
-				    except User.DoesNotExist:
-					    user = User.create(name = item['user'])
-                if item['topic']:
-                    # get TOPIC
-				    try:
-					    topic=Topic.get(Topic.name == item['topic'])
-				
-				    # no Topic? create one!
-				    except Topic.DoesNotExist:
-					    topic= Topic.create(name = itemitem['topic'])
-                  
-                     try:
-                        post=Post.get(Post.text==text, timestamp==timestamp)
-
-                    except Post.DoesNotExist:
-                        post=Post.create(text=text, timestamp=timestamp, order=order, topic=topic, user=user) #
-
-                      
-                else:
-                    raise DropItem("No topic")
-                    
+        if not item['user']:
+            raise DropItem("No user")
+        else:
+            # get USER
+            try:
+			    user=User.get(User.name == item['user'])
+		
+		    # no USER? create one!
+            except User.DoesNotExist:
+                user = User.create(name = item['user'])
+            if not item['topic']:
+                raise DropItem("No topic")
             else:
-                raise DropItem("No user")
+                # get TOPIC
+                try:
+                    topic=Topic.get(Topic.name == item['topic'])
+                except Topic.DoesNotExist:
+                    topic= Topic.create(name = item['topic'])
+                try:
+                    post=Post.get(Post.text==text, timestamp==timestamp)
 
-         return item
+                except Post.DoesNotExist:
+                    post=Post.create(text=text, timestamp=timestamp, order=order, topic=topic, user=user) 
+
+          
+                
+                
+       
+            
+
+        return item
 
        
        
