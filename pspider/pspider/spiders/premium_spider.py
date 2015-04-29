128a8bc23d22a49a55448f3cc82a996e212bc1a1
ninamiriamjnana <nina.pohler@posteo.de>
2015-04-29 16:33:08 +0200

    theroetisch hab ich hier einen spider gemacht der crawlt und immer bei topicpage ein item füllt und weitergibt

--- pspider/pspider/spiders/premium_spider.py
+++ pspider/pspider/spiders/premium_spider.py
@@ -1,25 +1,45 @@
 import scrapy
 
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+
 from pspider.items import PostItem
 
-class LoginSpider(scrapy.Spider):
+class LoginSpider(CrawlSpider):
     name = 'premium_login'
-    start_urls = ['https://www.premium-cola.de/board/ucp.php?mode=login']
+    login_page='https://www.premium-cola.de/board/ucp.php?mode=login'
+    allowed_domains=['premium-cola.de']
+    start_urls=['https://www.premium-cola.de/board/index.php?sid=d1b48640148a07ebceb8df77d8087c35']
 
-    
-    def __init__(self, username="", password="", *args, **kwargs):
-    	super(LoginSpider, self).__init__(*args, **kwargs)
-    	self.username = username
-        self.password=password
+    rules = (
+         # Extract links matching 'category.php' (but not matching 'subsection.php')
+        # and follow links from them (since no callback means follow=True by default).
+        Rule(LinkExtractor(allow=('/board/viewforum.php\?f=306', ))),
+       
+
+        # Extract links matching 'item.php' and parse them with the spider's method parse_item
+        Rule(LinkExtractor(allow=(r'/board/viewforum.php\?f=[0-9]+\&t=[0-9]+', )), callback='parse_topicpage'),
+    )
+        
+
+
+    def start_requests(self):
+    yield Request(
+    url=self.login_page,
+    callback=self.login,
+    dont_filter=True
+    )
 	
-     
+    def login(self, response):
+    #"""Generate a login request."""
+    return FormRequest.from_response(response,
+            formdata={'username': self.username, 'password': self.password},
+            callback=self.check_login_response)     
 
-    def parse(self, response):
-        return [scrapy.FormRequest.from_response(response,
-                    formdata={'username': self.username, 'password': self.password},
-                    callback=self.after_login)]
+   
+    
 
-    def after_login(self, response):
+    def check_login_response(self, response):
         # check login succeed before going on
         if "Du hast ein fehlerhaftes Passwort angegeben" in response.body:
             print "fehlerhaftes passwort"
@@ -28,8 +48,7 @@ class LoginSpider(scrapy.Spider):
             print "fehler after name"
             return
         else:
-            return scrapy.Request(url="https://www.premium-cola.de/board/viewtopic.php?f=354&t=1230",
-               callback=self.parse_topicpage)
+            return Request(url="https://www.premium-cola.de/board/index.php?sid=d1b48640148a07ebceb8df77d8087c35")
         # continue scraping with authenticated session...
 
 
