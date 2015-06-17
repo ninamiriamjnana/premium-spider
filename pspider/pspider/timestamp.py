ef94bfff0db78830015d29f975e9b21c0bdcabe0
ninamiriamjnana <nina.pohler@posteo.de>
2015-06-17 14:43:02 +0200

    funktion um aus dem timestamp das datum in isoformat zu holen

---
+++ pspider/pspider/timestamp.py
@@ -0,0 +1,20 @@
+
+
+
+def get_date (timestamp):
+	month_dict={'Jan':1, 'Feb':2, 'Mär':3, 'Apr':4, 'Mai':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dez':12}
+
+	string_base=timestamp.split(',')
+	md=string_base[0]
+	month=md[6:9]
+	day=md[10:12]
+	
+	y=string_base[1]
+	year=y[1:5]
+
+	date=year
+	date+=('-')
+	date+=(month)
+	date+=('-')	
+	date+=(day)
+	return date
