de5d13cc0ee7dbf7bb2d4fc6bca8d3b8654761eb
ninamiriamjnana <nina.pohler@posteo.de>
2015-06-17 15:20:35 +0200

    date field added to post

--- pspider/pspider/peeweemodels.py
+++ pspider/pspider/peeweemodels.py
@@ -1,6 +1,6 @@
 from peewee import *
 
-mysql_db= MySQLDatabase('arachnophobia', host='localhost', user='nina', passwd='pw' )
+mysql_db= MySQLDatabase('arachnophobia', host='localhost', user='user')
 
 class MySQLModel (Model):
 	class Meta:
@@ -16,23 +16,27 @@ class User (MySQLModel):
 class Topic (MySQLModel):
 	name = CharField(index=True)
 
-class Post (MySQLModel):
-    pid=IntegerField(index=True)
-    text=CharField()
-    timestamp=CharField()
-    order=IntegerField()
-    user= ForeignKeyField (User)	
-    topic= ForeignKeyField (Topic)
-    htmlpage=ForeignKeyField (HTMLPage)
-
 class HTMLPage(MySQLModel):
       url=CharField(index=True)
       body=TextField ()
+
+class Post (MySQLModel):
+	pid=IntegerField(index=True)
+	text=CharField()
+	timestamp=CharField()
+	date=CharField()
+	order=IntegerField()
+	user= ForeignKeyField (User)	
+	topic= ForeignKeyField (Topic)
+	htmlpage=ForeignKeyField (HTMLPage)
+
+
 
 
 def create_tables():	
     User.create_table()
     Topic.create_table()    
-    Post.create_table() 
-    HTMLPage.create_table()   
+ 
+    HTMLPage.create_table()
+    Post.create_table()   
 	
