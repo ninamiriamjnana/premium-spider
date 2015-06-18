7ae4e3c989ce7bb17d52b55e290c76b6eb4e5a40
ninamiriamjnana <nina.pohler@posteo.de>
2015-06-18 15:46:34 +0200

    graph ist jetzt da aber als nächstes muss ich die nodes mit namen labeln

--- pspider/pspider/create_graphs.py
+++ pspider/pspider/create_graphs.py
@@ -1,6 +1,7 @@
 
 from peeweemodels import *
 import networkx as nx
+import ipdb
 
 
 
@@ -12,11 +13,12 @@ def create_u_dict():
 	user_list=[]
 	
 	for x in myquery:
+		#ipdb.set_trace()
 		if(x.id==start_id):
 			user_list.append(x.topic_id)
 			alte_id=x.id
 		else:
-			udict['alte_id']=user_list
+			udict[alte_id]=user_list
 			user_list=[]
 			user_list.append(x.topic_id)
 			start_id=x.id
@@ -27,19 +29,23 @@ def compare (G, udict, such_user, such_topic):
 	for k, v in udict.iteritems():
 		for vergleich_topic in v:
 			if vergleich_topic==such_topic and such_user != k:
-				if nx.path.bidirectional.djikstra(G, such_user, k):
-					G['such_user']['k']['weight']+=1
+				#ipdb.set_trace()
+				if G.has_edge(such_user,k):
+					G[such_user][k]['weight']+=1
 				else:
 					G.add_edge(such_user, k, weight=1)
 	return G
 	
 
-def create_u_graph(udict):
+def create_u_graph():
+	udict=create_u_dict()
 
 	G=nx.Graph()
 	
 	for k, v in udict.iteritems():
 		G.add_node(k)
 		for topic in v:
-			G=vergleich(G,udict,k, topic)
+			G=compare(G,udict,k, topic)
+
+	return G
 	
