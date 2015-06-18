ed190ccf4c1eb99c73915cf25051a7442123c7f9
ninamiriamjnana <nina.pohler@posteo.de>
2015-06-18 17:03:44 +0200

    jetzt haben die nodes auch usernamen!

--- pspider/pspider/create_graphs.py
+++ pspider/pspider/create_graphs.py
@@ -2,27 +2,39 @@
 from peeweemodels import *
 import networkx as nx
 import ipdb
+import collections
 
 
 
 def create_u_dict():
-	myquery=(User.select(User.id,Topic.id.alias('topic_id')).join(Post).join(Topic).naive().order_by(User.id))
+	myquery=(User.select(User.id, User.name, Topic.id.alias('topic_id')).join(Post).join(Topic).naive().order_by(User.id))
 	start=myquery[0]
 	start_id=start.id
 	udict={}
 	user_list=[]
+
+	uname_dict={}
+
 	
 	for x in myquery:
 		#ipdb.set_trace()
+		uname_dict[x.id]=x.name
 		if(x.id==start_id):
 			user_list.append(x.topic_id)
 			alte_id=x.id
+			
 		else:
 			udict[alte_id]=user_list
 			user_list=[]
 			user_list.append(x.topic_id)
 			start_id=x.id
-	return udict
+	#ipdb.set_trace()
+	dictcollection=collections.namedtuple('dictcollection', ['udict','uname_dict'])
+	dc=dictcollection(udict, uname_dict)
+	return dc
+
+
+
 
 
 def compare (G, udict, such_user, such_topic):
@@ -38,14 +50,23 @@ def compare (G, udict, such_user, such_topic):
 	
 
 def create_u_graph():
-	udict=create_u_dict()
+	dc=create_u_dict()
+	
+	udict=dc[0]
+	unamedict=dc[1]
 
 	G=nx.Graph()
 	
 	for k, v in udict.iteritems():
 		G.add_node(k)
+		name=unamedict[k]
+		G.node[k]['name']=name
+		# wie krieg ich jetzt den namen dazu?
 		for topic in v:
 			G=compare(G,udict,k, topic)
 
 	return G
+
+
+	
 	
